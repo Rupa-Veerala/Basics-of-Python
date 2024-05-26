c53533eb34e4485924d2392028b2fc51ec5cc864
Veerala-Rupa <veeralarupa@gmail.com>
2024-05-26 13:08:17 +0530

    first commit

---
+++ Variables&Datatypes.py
@@ -0,0 +1,20 @@
+#Variables
+name = "Rupa"
+v1 = "India"
+_v1 = "Suchi"
+A1 = "lavanya"
+print("My name is " + name)
+print("I am from " + v1)
+print("My frnd is " + _v1)
+print("My frnd is " + A1)
+
+#Data types
+string = "My name is Rupa"
+print(string)
+print(type(string))
+integer = 12
+print(integer)
+print(type(integer))
+float = 2.445
+print(float)
+print(type(float))
