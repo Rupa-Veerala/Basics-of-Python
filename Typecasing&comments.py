d76ef77b41b6776572824af8b997988905346899
Veerala-Rupa <veeralarupa@gmail.com>
2024-05-26 17:38:31 +0530

    type/comments

---
+++ Typecasing&comments.py
@@ -0,0 +1,21 @@
+#Type casting
+a = 14
+b = 3.49
+c = "Rupa"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#comments
+#for single comments use hashtag
+#Adding two numbers
+a = 2
+b =3
+print(a+b)
+#for multiple comments we use """  """
+""" a=adding
+    b=sub """
+
+
